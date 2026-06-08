42a6c310c6b2a869faf8c58a6be4b9ce652df1cd
karasuta <kargina.ts@gmail.com>
2026-06-08 11:10:40 +0300

    изменения по заданию ПР 7 модуля

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,27 +1,19 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
-// @todo: #4.3 — настроить компаратор
+export function initFiltering(elements) {
 
-const compare = createComparison(defaultRules);
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                const el = document.createElement('option');
+                el.textContent = name;
+                el.value = name;
+                return el;
+            }))
+        })
+    }
 
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-
-    Object.keys(indexes)                                    // Получаем ключи из объекта
-      .forEach((elementName) => {                        // Перебираем по именам
-        elements[elementName].append(                    // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
-                      .map(name => {                        // используйте name как значение и текстовое содержимое
-                    const option = document.createElement("option");
-                    option.value = name;                                  // @todo: создать и вернуть тег опции
-                    option.textContent = name;
-                    return option;
-                })
-        )
-    })
-
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+    const applyFiltering = (query, state, action) => {
         if (action && action.name === 'clear') {
             const button = action.element;
             if (!button) return data;
@@ -41,8 +33,69 @@ export function initFiltering(elements, indexes) {
             state.filters[fieldName] = '';
         }
             }
+    
+
+    const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
+                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
+                }
+            }
+        })
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу
+    }
+
+    return {
+        updateIndexes,
+        applyFiltering
+    }
+
+}
+// @todo: #4.3 — настроить компаратор
+
+//const compare = createComparison(defaultRules);
+
+//export function initFiltering(elements, indexes) {
+    // @todo: #4.1 — заполнить выпадающие списки опциями
+
+    //Object.keys(indexes)                                    // Получаем ключи из объекта
+      //.forEach((elementName) => {                        // Перебираем по именам
+        //elements[elementName].append(                    // в каждый элемент добавляем опции
+            //...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      //.map(name => {                        // используйте name как значение и текстовое содержимое
+                    //const option = document.createElement("option");
+                    //option.value = name;                                  // @todo: создать и вернуть тег опции
+                    //option.textContent = name;
+    //                 return option;
+    //             })
+    //     )
+    // })
+
+    //return (data, state, action) => {
+        // @todo: #4.2 — обработать очистку поля
+        // if (action && action.name === 'clear') {
+        //     const button = action.element;
+        //     if (!button) return data;
+
+        // const fieldName = button.getAttribute('data-field');
+        // if (!fieldName) return data;
+
+        // const parent = button.closest('.filter-wrapper');
+        // if (!parent) return data;
+
+        // const input = parent.querySelector('input, select');
+        // if (input) {
+        //     input.value = ''; 
+        // }
+
+        // if (state.filters && state.filters.hasOwnProperty(fieldName)) {
+        //     state.filters[fieldName] = '';
+        // }
+            //}
         // @todo: #4.5 — отфильтровать данные используя компаратор
 
-        return data.filter(row => compare(row, state));
-    }
-}
+        //return data.filter(row => compare(row, state));
+    //}
+//}
